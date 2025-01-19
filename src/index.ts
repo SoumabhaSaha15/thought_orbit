4686a1e0d399a3b91c97cb34c1ad53a79af1f833
S.Saha <soumabhasaha1509+github@gmail.com>
2025-01-19 10:35:38 +0530

    gridFS added and some optimization done

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,44 @@
-import CookieParser from "cookie-parser";
-import Router from "./routes/index.js";
+import { GridFSBucket } from 'mongodb';
+import cookieParser from "cookie-parser";
+import router from "./routes/index.js";
 import { connect } from "mongoose";
-import Express from "express";
+import express from "express";
 import dotenv from "dotenv";
 import chalk from "chalk";
 import path from "path";
 try {
   dotenv.config();
-  await connect(process.env.DB_URI as string);
-  Express()
-    .use(Express.static(path.join(import.meta.dirname, "./../public")))
-    .use(Express.json())
-    .use(Express.urlencoded({ extended: true }))
-    .use(CookieParser())
-    .use(Router)
+  const CONNECTOR = await connect(process.env.DB_URI as string);
+  const db = CONNECTOR.connection.db;
+  const gridFSBucket = db ? new GridFSBucket(db, { bucketName: process.env.GFSB_SRC as string }) : null;
+  if (!gridFSBucket) throw new Error('Failed to init GridFSBucket');
+  const APP = express()
+    .use(express.static(path.join(import.meta.dirname, "./../public")))
+    .use(express.json())
+    .use(express.urlencoded({ extended: true }))
+    .use(cookieParser())
+    .use(async (req:express.Request, _:express.Response, next:express.NextFunction) => {
+      req.GFSB = gridFSBucket; //Centralizing geid fs.
+      next();
+    })
+    .use(router)
     .set("view engine", "ejs")
     .set("views", path.join(import.meta.dirname, "./../views"))
     .listen(process.env.PORT, () => {
       console.log(chalk.blue.bold(`running http://localhost:${process.env.PORT}`));
     });
+  process.on("unhandledRejection", (reason) => {
+    console.log(chalk.red.bold("Unhandled Rejection:"), '\n', reason);
+  });
+  //Graceful shutdown.
+  process.on("SIGINT", async () => {
+    console.log(chalk.yellow.bold("Gracefully shutting down..."));
+    await CONNECTOR.disconnect();
+    APP.close(() => {
+      console.log(chalk.green.bold("Server closed. MongoDB disconnected."));
+      process.exit(0);
+    });
+  });
 } catch (error) {
   console.log(chalk.red.bold((error as Error).message));
 }
---
+++ src/types/express.d.ts
@@ -0,0 +1,8 @@
+import { GridFSBucket } from 'mongodb';
+declare global {
+  namespace Express {
+    interface Request {
+      GFSB: GridFSBucket;
+    }
+  }
+}
