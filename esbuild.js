182591ff82e424ace725cec3e3616dbb4bc6b25d
S.Saha <soumabhasaha1509+github@gmail.com>
2025-01-30 20:43:33 +0530

    esbuild updated

--- esbuild.js
+++ esbuild.js
@@ -1,6 +1,25 @@
+import path from 'path';
 import esbuild from "esbuild";
-import { esbuildConfig } from "./esbuild.config.js";
 import { rainbow } from "gradient-string";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+export const esbuildConfig = {
+  entryPoints: [
+    path.resolve("public/imports/**/*.js"),
+    path.resolve("public/imports/tiptap/core/*.js"),
+    path.resolve("public/imports/tiptap/extension/*.js"),
+    path.resolve("public/imports/tiptap/*.js"),
+    path.resolve("public/imports/flowbite/*.js"),
+  ], // Include all JS files
+  outdir: "public/bundle", // Output directory for code-split files
+  bundle: true, // Enable bundling
+  minify: false, // Minify files
+  sourcemap: false, // No sourcemaps
+  format: "esm", // Keep ES module format
+  target: "esnext", // Target modern JS
+  splitting: true, // Enable code-splitting
+}
 esbuild
   .build(esbuildConfig)
   .then(() => console.log(rainbow("updated the changes in public/imports/")))
