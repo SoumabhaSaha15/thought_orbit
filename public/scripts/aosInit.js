b79f12d31226fff6b634b745637b34d3a636d973
S.Saha <soumabhasaha1509+github@gmail.com>
2025-01-31 09:56:54 +0530

    ion-icon done

---
+++ public/scripts/aosInit.js
@@ -0,0 +1,10 @@
+import AOS from './../bundle/aos.js';
+AOS.init({
+  offset: 120,
+  delay: 0,
+  duration: 400,
+  easing: 'ease',
+  once: true,
+  mirror: false,
+  anchorPlacement: 'top-bottom'
+});
